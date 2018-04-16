94d9c6142c31b3187910ba2161d690e0508b3791
wogua <lijun@lijundeMacBook-Pro.local>
2018-04-16 22:36:15 +0800

    for badge

--- badge/src/com/android/zylauncher/badge/BadgeInfo.java
+++ badge/src/com/android/zylauncher/badge/BadgeInfo.java
@@ -1,5 +1,7 @@
 package com.android.zylauncher.badge;
 
+import com.android.launcher3.notification.PackageUserKey;
+
 /**
  * Created by lijun on 17-9-20.
  */
@@ -12,8 +14,14 @@ public class BadgeInfo {
     public int creator;
     public String lastModifyTime;
 
+    public BadgeInfo(PackageUserKey postedPackageUserKey) {
+
+    }
+
     @Override
     public String toString() {
         return "id:" + id + ", pkgName:" + pkgName + ", shortcutCustomId:" + shortcutCustomId + ", badgeCount:" + badgeCount + ", creator:" + creator + ", lastModifyTime:" + lastModifyTime;
     }
+
+
 }
